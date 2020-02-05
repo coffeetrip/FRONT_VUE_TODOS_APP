58f9c34e5d74ed024eb426bf6866526e8a60c95e
양예은 <yye7926@naver.com>
2020-02-05 23:15:54 +0900

    [+] TodoList 기본 구조 추가

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>todo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      todos: []
+    };
+  }
+};
+</script>
+
+<style>
+</style>
