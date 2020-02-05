901f78b3888d5beef13e182a41667e8376b5edf8
양예은 <yye7926@naver.com>
2020-02-05 23:10:56 +0900

    [+] token session save update

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,6 +4,9 @@
 			<span class="sr-only">Loading...</span>
 		</div>
 		<div v-else class="login-div col-6 offset-3">
+			<div v-if="errors.length" class="error-list alert alert-danger">
+        <div v-for="(error,idx) in errors" :key="idx">{{error}}</div>
+      </div>
 			<div class="form-group">
 				<label for="id">ID</label>
 				<input class="form-control" type="text" id="id" v-model="credential.username" />
@@ -29,26 +32,41 @@ export default {
         username: "",
         password: ""
       },
-      loading: false
+			loading: false,
+			errors: []
     };
   },
   methods: {
     login() {
-      console.log("로그인시도");
-      axios
-        .post("http://localhost:8000/accounts/login/", this.credential)
-        .then(response => {
-					// 토큰 저장
-          const { token } = response.data;
-          this.$session.start(); // vue-session때문에 생김
-          this.$session.set("jwt", token); // 첫번째 매개변수 이름으로 token을 저장.
-          // 리다이렉트
-          router.push("/");
-        })
-				.catch(error => {
-					console.log(error)
-					this.loading = true; 
-					});				
+			if (this.checkForm()) {
+				console.log("로그인시도");
+				axios
+					.post("http://localhost:8000/accounts/login/", this.credential)
+					.then(response => {
+						this.loading = true;
+						// 토큰 저장
+						const { token } = response.data;
+						this.$session.start(); // vue-session때문에 생김
+						this.$session.set("jwt", token); // 첫번째 매개변수 이름으로 token을 저장.
+						// 리다이렉트
+						router.push("/");
+					})
+					.catch(error => {
+						console.log(error)
+						});			
+			}	
+		},
+		checkForm() {
+			this.errors = [];
+      if (this.credential.password.length < 8) {
+        this.errors.push("비밀번호는 8글자가 넘어야 합니다.");
+      }
+      if (!this.credential.username) {
+        this.errors.push("아이디를 입력 해주세요");
+      }
+      if (this.errors.length === 0) {
+        return true;
+			}
     }
   }
 };
