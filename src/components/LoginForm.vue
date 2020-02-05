27e96f6085b5f6f8c4b6eb5f1c27a9b04ccd574a
양예은 <yye7926@naver.com>
2020-02-05 21:47:00 +0900

    [+] login axios

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,19 +1,51 @@
 <template>
-  <div class="login-div">
-    <div class="form-group">
-      <label for="id">ID</label>
-      <input class="form-control" type="text" id="id" />
-    </div>
-    <div class="form-group">
-      <label for="pwd">PASSWORD</label>
-      <input class="form-control" type="password" id="pwd" />
-    </div>
-    <button class="btn btn-primary">login</button>
+	<div>
+		<div v-if="loading" class="spinner-border" role="status">
+			<span class="sr-only">Loading...</span>
+		</div>
+		<div class="login-div">
+			<div class="form-group">
+				<label for="id">ID</label>
+				<input class="form-control" type="text" id="id" v-model="credential.username" />
+			</div>
+			<div class="form-group">
+				<label for="pwd">PASSWORD</label>
+				<input class="form-control" type="password" id="pwd" v-model="credential.password" />
+			</div>
+			<button class="btn btn-primary" @click="login">login</button>
+		</div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      credential: {
+        username: "",
+        password: ""
+      },
+      loading: false
+    };
+  },
+  methods: {
+    login() {
+      console.log("로그인시도");
+      axios
+        .post("http://localhost:8000/accounts/login/", this.credential)
+        .then(response => {
+          console.log(response);
+        })
+				.catch(error => {
+					console.log(error)
+					this.loading = true; 
+					});				
+    }
+  }
+};
 </script>
 
 <style>
