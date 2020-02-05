bb8c2a83927aeba7e527a93bbad49d6a1ae1e5e5
양예은 <yye7926@naver.com>
2020-02-06 00:02:53 +0900

    [+] login 오류 막기

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,17 +4,16 @@
 			<span class="sr-only">Loading...</span>
 		</div>
 		<div v-else class="login-div col-6 offset-3">
-			<div v-if="errors.length" class="error-list alert alert-danger">
-        <div v-for="(error,idx) in errors" :key="idx">{{error}}</div>
-      </div>
+
 			<div class="form-group">
-				<label for="id">ID</label>
-				<input class="form-control" type="text" id="id" v-model="credential.username" />
+				<label for="username">ID</label>
+				<input id="username" class="form-control" type="text"  @keyup="onUsername" placeholder="사용자 이름">
 			</div>
 			<div class="form-group">
-				<label for="pwd">PASSWORD</label>
-				<input class="form-control" type="password" id="pwd" v-model="credential.password" />
+				<label for="password">PASSWORD</label>
+				<input id="password" class="form-control" type="password" @keyup="onPassword" placeholder="비밀번호">
 			</div>
+			<p v-if="errorMessage.length > 0" class="text-center" style="color:red">{{ errorMessage }}</p>
 			<button class="btn btn-primary" @click="login">login</button>
 		</div>
   </div>
@@ -33,40 +32,37 @@ export default {
         password: ""
       },
 			loading: false,
-			errors: []
+			errorMessage: ""
     };
   },
   methods: {
     login() {
-			if (this.checkForm()) {
-				console.log("로그인시도");
-				axios
-					.post("http://localhost:8000/accounts/login/", this.credential)
-					.then(response => {
-						this.loading = true;
-						// 토큰 저장
-						const { token } = response.data;
-						// this.$session.start(); // vue-session때문에 생김
-						this.$store.dispatch("login", token); 
-						// 리다이렉트
-						router.push("/");
-					})
-					.catch(error => {
-						console.log(error)
-						});			
-			}	
-		},
-		checkForm() {
-			this.errors = [];
-      if (this.credential.password.length < 8) {
-        this.errors.push("비밀번호는 8글자가 넘어야 합니다.");
-      }
-      if (!this.credential.username) {
-        this.errors.push("아이디를 입력 해주세요");
-      }
-      if (this.errors.length === 0) {
-        return true;
-			}
+			console.log("로그인시도");
+			axios
+				.post("http://localhost:8000/accounts/login/", this.credential)
+				.then(response => {
+					this.loading = true;
+					// 토큰 저장
+					const { token } = response.data;
+					// this.$session.start(); // vue-session때문에 생김
+					this.$store.dispatch("login", token); 
+					// 리다이렉트
+					router.push("/");
+				})
+				.catch(error => {
+					this.loginAlert();
+					console.log(error)
+					});		
+		},	
+		loginAlert() {
+      this.errorMessage =
+        "아이디 또는 비밀번호가 올바르지 않습니다.";
+    },
+    onUsername(e) {
+      this.credential.username = e.target.value;
+    },
+    onPassword(e) {
+      this.credential.password = e.target.value;
     }
   }
 };
