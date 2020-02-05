ca0398d162494f56834df5681af9620a9a4bc46d
양예은 <yye7926@naver.com>
2020-02-05 21:38:06 +0900

    [+] login 기능 구현

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login-div">
+    <div class="form-group">
+      <label for="id">ID</label>
+      <input class="form-control" type="text" id="id" />
+    </div>
+    <div class="form-group">
+      <label for="pwd">PASSWORD</label>
+      <input class="form-control" type="password" id="pwd" />
+    </div>
+    <button class="btn btn-primary">login</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>로그인페이지 입니다.</h1>
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from "../components/LoginForm";
+export default {
+  name: "Login",
+  components: {
+    LoginForm
+  }
+};
+</script>
+
+<style>
+</style>
