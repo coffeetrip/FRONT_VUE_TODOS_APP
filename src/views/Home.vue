769a2aaf0a955d1542066b9babf8c8481981c42b
양예은 <yye7926@naver.com>
2020-02-05 23:14:30 +0900

    라이프사이클 mounted 로그인 검증

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todos</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import router from "../router";
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
+  },
+  methods: {
+    checkLoggedIn() {
+      this.$session.start();
+      if (!this.$session.has("jwt")) {
+        router.push("/login");
+      }
+    }
+  },
+  mounted() {
+    this.checkLoggedIn();
   }
 }
 </script>
